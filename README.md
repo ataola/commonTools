dac1853c24befcc732da431410018c2e40aacfcc
ataola <lovejiangtao97@163.com>
2020-05-21 16:10:37 +0800

    valueType formatSentence

--- README.md
+++ README.md
@@ -98,6 +98,63 @@ const {utils} = require('zjtcommonTools');
     console.log('after sleep 3 second!');
 })();
 ```
+* formatSentence(str, { initials, upper, lower})  大小写转换函数
 
+initials是否支持首字母大小写， upper大写， lower小写
+
+```js
+const {utils} = require('zjtcommonTools');
+const str = 'ataola';
+const str2 = 'hello world';
+const str3 = 'ATAOLA';
+
+console.log(utils.formatSentence(str, {
+    initials: false,
+    upper: true,
+    lower: false
+}));
+
+console.log(utils.formatSentence(str, {
+    initials: false,
+    upper: false,
+    lower: true
+}));
+
+console.log(utils.formatSentence(str3, {
+    initials: false,
+    upper: false,
+    lower: true
+}));
+
+console.log(utils.formatSentence(str3, {
+    initials: true,
+    upper: false,
+    lower: true
+}));
+
+console.log(utils.formatSentence(str2, {
+    initials: true,
+    upper: true,
+    lower: false
+}));
+```
+* valueType(value) 值类型检测
+
+```js
+const {utils} = require('zjtcommonTools');
+const str = 'hello world';
+const num = 2020;
+const bool = true;
+const obj = {name: 'ataola'};
+const arr = [2, 0, 2, 0];
+const nuo = null;
+function func() {};
+
+const test = [str, num, bool, obj, arr, func, nuo];
+
+for(const val of  test) {
+    console.log(`${val} is ${utils.valueType(val)}`);
+}
+```
 
 
--- utils/index.js
+++ utils/index.js
@@ -1,24 +1,78 @@
 class Util {
 
-    sleep(millisecond) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve();
-            }, millisecond);
-        });
-    }
+	_transformSentence(str, {
+		upper,
+		lower
+	}) {
+		if (upper) {
+			return str.toUpperCase();
+		}
 
-    sleepNotRecommended(millisecond) {
-        let start = Date.now();
-        while (Date.now() - start < millisecond) {
-            // donothing
-            if (Date.now() - start >= millisecond) break;
-        }
-    }
+		if (lower) {
+			return str.toLowerCase();
+		}
 
-    static createUtil() {
-        return new Util();
-    }
+		return str;
+	}
+
+	sleep(millisecond) {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve();
+			}, millisecond);
+		});
+	}
+
+	sleepNotRecommended(millisecond) {
+		let start = Date.now();
+		while (Date.now() - start < millisecond) {
+			// donothing
+			if (Date.now() - start >= millisecond) break;
+		}
+	}
+
+
+
+	formatSentence(str, {
+		initials,
+		upper,
+		lower
+	} = {}) {
+		if (initials) {
+			if (upper) {
+				return str.replace(/\b\w+\b/g, function (word) {
+					return word.substring(0, 1).toUpperCase() + word.substring(1);
+				});
+			}
+
+			if (lower) {
+				return str.replace(/\b\w+\b/g, function (word) {
+					return word.substring(0, 1).toLowerCase() + word.substring(1);
+				});
+			}
+			return str;
+		}
+
+		return this._transformSentence(str, {
+			upper,
+			lower
+		});
+	}
+
+	valueType(value) {
+		// 值类型  Number、String、Boolean、Object、Function、Null
+		const type = typeof value;
+		if (type === 'object') {
+			if (value instanceof Array) return 'array';
+			if (value === null) return 'null';
+			return 'object';
+		}
+		return type;
+	}
+
+	static createUtil() {
+		return new Util();
+	}
 }
 
 module.exports = Util.createUtil();
