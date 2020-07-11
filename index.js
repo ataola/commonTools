8b75b2251fb1144a0fa9864a5f88ea4277b2026b
ataola <zhengjiangtao@citytogo.com>
2020-07-11 13:47:45 +0800

    add validate

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
  * @Author: ataola 
  * @Date: 2020-04-01 00:59:59 
  * @Descripton: 一个Javascript工具库，一个原则是尽量自己实现，不借助第三方库
- * @Last Modified by: ataola
- * @Last Modified time: 2020-05-06 18:58:18
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2020-07-11 11:34:02
  */
 
 
 const random = require('./random')
 const date = require('./date');
 const utils = require('./utils');
+const validate = require('./validate');
 
  module.exports = {
      random,
      date,
-     utils
+     utils,
+     validate,
  }
